28be3bc39e8e4847f77cbe06fa17c906b4fe10ab
Marcello Seri <marcello.seri@gmail.com>
2019-08-06 18:48:08 +0200

    make second example customisable
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/preypredator.ml
+++ src/preypredator.ml
@@ -14,7 +14,7 @@ let y0' = D.of_array [| 10.0; 10.0 |] [| 2; 1 |]
 
 let integrate coefficients y0 =
   let open Owl_ode_base in
-  let tspec = Types.T1 { t0 = 0.0; dt = 1E-2; duration = 100.0 } in
+  let tspec = Types.T1 { t0 = 0.0; dt = 1E-2; duration = 20.0 } in
   let model = lotka_volterra coefficients in
   let module Native = Native_generic.Make (Owl_base_dense_ndarray.D) in
   Ode.odeint Native.euler model y0 tspec ()
@@ -47,11 +47,27 @@ let () =
   )
 *)
 
+let prepare coefficients y0 =
+  let ts, ys = integrate coefficients y0 in
+  let to_list arr =
+    let rec go acc idx =
+      if idx <= 0 then acc else go (D.get arr [| 0; idx - 1 |] :: acc) (idx - 1)
+    in
+    go [] (D.shape arr).(1)
+  in
+  let ts = to_list (D.get_slice [ [ 0 ]; [ 0; -1; 10 ] ] ts) in
+  let xs = to_list (D.get_slice [ [ 0 ]; [ 0; -1; 10 ] ] ys) in
+  let ys = to_list (D.get_slice [ [ 1 ]; [ 0; -1; 10 ] ] ys) in
+  List.combine ts xs, List.combine ts ys
+
+
 open Js_of_ocaml
+module Html = Dom_html
+
+let document = Html.window##.document
 
 let get_by_id id =
-  let d = Dom_html.document in
-  Js.Opt.get (d##getElementById (Js.string id)) (fun () -> assert false)
+  Js.Opt.get (document##getElementById (Js.string id)) (fun () -> assert false)
 
 
 let plotlv predator prey name =
@@ -64,24 +80,37 @@ let plotlv predator prey name =
       ]
 
 
-let _ =
-  let prepare coefficients y0 =
-    let ts, ys = integrate coefficients y0 in
-    let to_list arr =
-      let rec go acc idx =
-        if idx <= 0 then acc else go (D.get arr [| 0; idx - 1 |] :: acc) (idx - 1)
-      in
-      go [] (D.shape arr).(1)
-    in
-    let ts = to_list (D.get_slice [ [ 0 ]; [ 0; -1; 10 ] ] ts) in
-    let xs = to_list (D.get_slice [ [ 0 ]; [ 0; -1; 10 ] ] ys) in
-    let ys = to_list (D.get_slice [ [ 1 ]; [ 0; -1; 10 ] ] ys) in
-    List.combine ts xs, List.combine ts ys
+let redrawer (alpha, beta, gamma, delta) =
+  let alpha, beta, gamma, delta = ref alpha, ref beta, ref gamma, ref delta in
+  let redraw () =
+    let predator', prey' = prepare (!alpha, !beta, !gamma, !delta) y0' in
+    plotlv predator' prey' "#volterralotka1"
   in
+  let assoc value name =
+    let input =
+      match Html.getElementById_coerce name Html.CoerceTo.input with
+      | Some input -> input
+      | None -> raise (Failure ("Input " ^ name ^ " not found."))
+    in
+    input##.value := Js.string (string_of_float !value);
+    input##.onchange
+      := Html.handler (fun _ ->
+             (try value := float_of_string (Js.to_string input##.value) with
+             | Invalid_argument _ -> ());
+             input##.value := Js.string (string_of_float !value);
+             redraw ();
+             Js._false)
+  in
+  List.iter
+    (fun (n, v) -> assoc v n)
+    [ "alpha", alpha; "beta", beta; "gamma", gamma; "delta", delta ];
+  redraw ()
+
+
+let _ =
   let predator, prey = prepare coefficients y0 in
-  let predator', prey' = prepare coefficients' y0' in
   Dom_html.window##.onload
     := Dom_html.handler (fun _ ->
            plotlv predator prey "#volterralotka";
-           plotlv predator' prey' "#volterralotka1";
+           redrawer coefficients';
            Js._true)
