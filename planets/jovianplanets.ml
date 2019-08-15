f87084d6df863f427f1bc49d5e14fb759595bca3
Marcello Seri <marcello.seri@gmail.com>
2019-08-15 17:39:31 +0200

    plot points instead
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- planets/jovianplanets.ml
+++ planets/jovianplanets.ml
@@ -3,12 +3,52 @@
     N-Body Simulation." White Paper, Colfax International. *)
 open Owl
 
+module FloatOps = struct
+  let (+), (-), ( * ), (/) = (+.), (-.), ( *. ), (/.)
+end
+
+
+(** Color palette helper *)
+let hsl_to_rgb hue saturation lightness =
+  let open FloatOps in
+  let degrees a = a * Float.pi / 180.0 in
+  let chroma = (1.0 - abs_float (2.0 * lightness - 1.0)) * saturation in
+  let hue = hue / (degrees 60.0) in
+  let x = chroma * (1.0 - abs_float ((mod_float hue 2.) - 1.0)) in
+  let r, g, b = match hue with
+    | hue when hue < 0.0 -> (0.0, 0.0, 0.0)
+    | hue when hue < 1.0 -> (chroma, x, 0.0)
+    | hue when hue < 2.0 -> (x, chroma, 0.0)
+    | hue when hue < 3.0 -> (0.0, chroma, x)
+    | hue when hue < 4.0 -> (0.0, x, chroma)
+    | hue when hue < 5.0 -> (x, 0.0, chroma)
+    | hue when hue < 6.0 -> (chroma, 0.0, x)
+    | _ -> (0.0, 0.0, 0.0)
+  in
+  let m = lightness - chroma / 2.0 in
+  (r + m, g + m, b + m)
+
+let get_colors n =
+  let rec go acc = function
+    | idx when idx < 0 -> acc
+    | idx ->
+      let open FloatOps in
+      let h = 1./.(float_of_int n) in
+      let s = (90.0 +. Random.float 10.)/.100. in
+      let l = (50.0 +. Random.float 10.)/.100. in
+      let r,g,b =
+        hsl_to_rgb h s l 
+        |> fun (r,g,b) -> int_of_float (255.0*r), int_of_float (255.0*g), int_of_float (255.0*b)
+      in go (Owl_plplot.Plot.RGB (r,g,b) :: acc) Stdlib.(idx-1)
+  in go [] (n-1) |> Array.of_list
+
+
 let n_particles = 1000
+let p_colors = get_colors n_particles
 
 let planets  = Mat.gaussian n_particles 3
 let planetvs = Mat.zeros n_particles 3
 let shift = Mat.create n_particles 1 1E-10
-
 
 let nbody_np planets =
   let force = Mat.zeros n_particles 3 in
@@ -42,6 +82,39 @@ let () =
   let dt = 0.01 in
   let state = ref (planets, planetvs) in
   Printf.printf "%.9f\n" (energy planets planetvs);
-  for _ = 1 to n do state := fst @@ advance dt !state done;
-  let planets, planetevs = !state in
+  let h = Owl_plplot.Plot.create ~n:1 ~m:3 "planets.png" in
+  let spec large c = Owl_plplot.Plot.[c; MarkerSize (if large then 1.5 else 0.5); Marker "."] in
+  for _ = 1 to n do
+    state := fst @@ advance dt !state;
+
+    let planets = fst !state in
+    Array.iteri (fun i c ->
+        let spec = spec (i=n) c in
+
+        let open Owl_plplot.Plot in
+
+        subplot h 0 0;
+        set_foreground_color h 0 0 0;
+        set_background_color h 255 255 255;
+        set_xlabel h "x";
+        set_ylabel h "y";
+        scatter ~h ~spec planets.Mat.${[[i];[0]]} planets.Mat.${[[i];[1]]};
+
+        subplot h 1 0;
+        set_foreground_color h 0 0 0;
+        set_background_color h 255 255 255;
+        set_xlabel h "y";
+        set_ylabel h "z";
+        scatter ~h ~spec planets.Mat.${[[i];[1]]} planets.Mat.${[[i];[2]]};
+
+        subplot h 2 0;
+        set_foreground_color h 0 0 0;
+        set_background_color h 255 255 255;
+        set_xlabel h "x";
+        set_ylabel h "z";
+        scatter ~h ~spec planets.Mat.${[[i];[0]]} planets.Mat.${[[i];[2]]};
+      ) p_colors;
+  done;
+  Owl_plplot.Plot.output h;
+  let planets, planetvs = !state in
   Printf.printf "%.9f\n" (energy planets planetvs)
