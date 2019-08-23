ce56a6a8c820d6eba2cc11c1b9a3f5034fe62e48
Marcello Seri <marcello.seri@gmail.com>
2019-08-23 14:24:52 +0200

    ocamlformat step
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- teaching_material/plot_bindings.ml
+++ teaching_material/plot_bindings.ml
@@ -136,10 +136,18 @@ let plotlv_plotly name ?phasedata ts predator prey =
 
 let plotlv_xkcd svg ts predator prey =
   let xkcd_line = Js.Unsafe.variable "chartXkcd.XY" in
-  let xymapper ys = Js.array @@
-    Array.map2 (fun t y ->
-      object%js val x = t val y = y end
-    ) ts ys in
+  let xymapper ys =
+    Js.array
+    @@ Array.map2
+         (fun t y ->
+           object%js
+             val x = t
+
+             val y = y
+           end)
+         ts
+         ys
+  in
   let plot =
     object%js
       val title = Js.string "Prey-Predator system"
@@ -151,28 +159,30 @@ let plotlv_xkcd svg ts predator prey =
       val data =
         object%js
           (* val labels = Js.array ts *)
-
           val datasets =
             Js.array
               [| object%js
                    val label = Js.string "Prey"
 
-                   val data = (xymapper prey)
+                   val data = xymapper prey
                  end
                ; object%js
                    val label = Js.string "Predator"
 
-                   val data = (xymapper predator)
+                   val data = xymapper predator
                  end
               |]
         end
 
-      val options= object%js
-      val xTickCount = 6
-      (* val legendPosition = chartXkcd.config.positionType.upLeft, *)
-      val showLine = Js.bool true
-      val dotSize = 0.0
-    end
+      val options =
+        object%js
+          val xTickCount = 6
+
+          (* val legendPosition = chartXkcd.config.positionType.upLeft, *)
+          val showLine = Js.bool true
+
+          val dotSize = 0.0
+        end
     end
   in
   new%js xkcd_line svg plot
